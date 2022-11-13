c0a6bed2bb3bb55e1d4eefbb098460d8ed2cc9b8
hemjay07 <mujeebopabode07@gmail.com>
2022-11-13 12:05:07 +0100

    feat : created a react app for the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
