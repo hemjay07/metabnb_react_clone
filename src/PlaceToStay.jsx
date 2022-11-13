60f7f256d3a0bab28519cb63733ae5d0e462bf21
hemjay07 <mujeebopabode07@gmail.com>
2022-11-14 00:05:25 +0100

    feat: created a PlaceToStay component which represents the second page of the app

---
+++ src/PlaceToStay.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Places from "../components/Places";
+export default function PlaceToStay() {
+  return (
+    <>
+      <Header />
+      <Places />
+      <Footer />
+    </>
+  );
+}
