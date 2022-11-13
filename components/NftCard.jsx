08b68312c2fe7735ae875d8912748dc8a3ff0a7f
hemjay07 <mujeebopabode07@gmail.com>
2022-11-13 12:16:18 +0100

    feat: added the NftCard component

---
+++ components/NftCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Card(prop) {
+  return (
+    <>
+      <div className="card">
+        {" "}
+        <img src={prop.image} alt="nft card" />
+        <div className="nft--info">
+          <p className="nft--info--1">Desert king</p>
+          <p>
+            <b>1MBT per night</b>
+          </p>
+        </div>
+        <div className="nft--info">
+          <p className="nft--info--1">2345km away</p>
+          <p>available for 2weeks stay</p>
+        </div>
+        <img className="stars" src="./stars.svg" alt="" />{" "}
+      </div>
+    </>
+  );
+}
