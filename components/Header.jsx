74afb0b0e0d23aa61af731476ff3f52d6e054c9d
hemjay07 <mujeebopabode07@gmail.com>
2022-11-16 00:14:47 +0100

    update: implemented modal in the header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,13 +7,22 @@ import Col from "react-bootstrap/Col";
 import { Link } from "react-router-dom";
 export default function () {
   const [Mobile, setMobile] = React.useState("nav--mobile");
-
+  const [modal, setModal] = React.useState(false);
+  function handleModal() {
+    setModal(!modal);
+  }
   function handleclick() {
     if (Mobile == "") {
       setMobile("nav--mobile");
     } else {
       setMobile("");
     }
+  }
+  // This statement helps prevent scroll of the entire page if the connect modal is up
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
   }
   return (
     <Container>
@@ -23,6 +32,40 @@ export default function () {
           <div className="lost">
             {" "}
             <img className="logo" src="/Group.svg" />
+            {modal && (
+              <>
+                <div className="connect">
+                  <div className="modal--title">
+                    <p>Connect Wallet</p>{" "}
+                    <img onClick={handleModal} src="./x.svg" />
+                  </div>
+                  <p>Choose your prefered wallet: </p>
+                  <div className="connect--buttons">
+                    <Button className="connect--button">
+                      <div>
+                        <img
+                          className="connect--logo"
+                          src="./MetamaskConnect.svg"
+                        />
+                        <span className="connect--name">Metamask</span>{" "}
+                      </div>
+                      <span> {">"} </span>
+                    </Button>
+                    <Button className="connect--button">
+                      <div>
+                        <img
+                          className="connect--logo"
+                          src="./walletConnect.svg"
+                        />
+                        <span className="connect--name">WalletConnect</span>
+                      </div>
+                      <span> {">"} </span>
+                    </Button>
+                  </div>
+                </div>
+                <div onClick={handleModal} className="overlay"></div>
+              </>
+            )}
           </div>
         </Col>
         <Col className="desktop--nav">
@@ -40,7 +83,9 @@ export default function () {
         </Col>
         <Col xs="auto">
           {" "}
-          <Button className="navv--button">conect wallet</Button>
+          <Button onClick={handleModal} className="navv--button">
+            conect wallet
+          </Button>
         </Col>
         <Col xs="auto">
           {" "}
@@ -72,7 +117,9 @@ export default function () {
             <li>Community</li>
           </ul>
 
-          <Button className="navv--button--mobile">conect wallet</Button>
+          <Button onClick={handleModal} className="navv--button--mobile">
+            conect wallet
+          </Button>
         </Col>
       </Row>
     </Container>
